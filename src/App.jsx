21d34c0f50f82902f144d92ffb196eba5a695d38
Esteak Ahmed Adnan <96421546+Adnan-Ahmed07@users.noreply.github.com>
2024-12-17 13:49:22 -0700

    Adding footer so no one can't copy my website

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Skills from './components/Skills';
 import Projects from './components/Project';
 import Contact from './components/Contact';
+import Aos from "aos";
+import "aos/dist/aos.css"
+import Footer from "./components/Footer";
 const App=()=>{ 
+  useEffect(() => {
+    Aos.init();
+    
+   }, [])
+ 
+
+
+
   return (
   <>
   <Navbar></Navbar>
@@ -13,6 +24,7 @@ const App=()=>{
   <Skills></Skills>
   <Projects></Projects>
   <Contact></Contact>
+  <Footer></Footer>
   </div>
   </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer=()=>{ 
+  return(
+   <footer>
+
+<div className="copyright">
+
+            © 2024 https://github.com/Adnan-Ahmed07. All rights reserved.
+        </div>
+   
+   </footer>
+
+  );
+}
+export default Footer;
