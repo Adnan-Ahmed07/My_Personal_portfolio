a793e0b33169d5f331648968bfe8426220b271a3
Esteak Ahmed Adnan <96421546+Adnan-Ahmed07@users.noreply.github.com>
2024-12-17 06:30:43 -0700

    Skills page working correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Skills from './components/Skills';
 const App=()=>{ 
   return (
   <>
   <Navbar></Navbar>
+  <div className="container">
   <Home></Home>
+  <Skills></Skills>
+  </div>
   </>
   )
 }
